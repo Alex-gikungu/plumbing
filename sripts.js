2d040ec473f8f96e34da4a5d455bc003bed13a02
Alex-gikungu <alexigikungu.012@gmail.com>
2023-07-15 22:02:48 +0300

    updated css file

--- sripts.js
+++ sripts.js
@@ -1,23 +1,72 @@
-let slides = document.querySelectorAll('.slide');
-let currentIndex = 0;
+// let slides = document.querySelectorAll('.slide');
+// let currentIndex = 0;
 
-function showNextSlide() {
-    slides[currentIndex].classList.remove('active');
-    currentIndex = (currentIndex + 1) % slides.length;
-    slides[currentIndex].classList.add('active');
-}
+// function showNextSlide() {
+//     slides[currentIndex].classList.remove('active');
+//     currentIndex = (currentIndex + 1) % slides.length;
+//     slides[currentIndex].classList.add('active');
+// }
 
-setInterval(showNextSlide, 5000); // Adjust the slide duration (in milliseconds) as needed
+// setInterval(showNextSlide, 5000); // Adjust the slide duration (in milliseconds) as needed
 
-let heading = document.querySelector("#list");
+// let heading = document.querySelector("#list");
 
-heading.addEventListener("mouseover",()=>{
-    heading.style.backgroundColor="green";
-})
-
-
+// heading.addEventListener("mouseover",()=>{
+//     heading.style.backgroundColor="green";
+// })
 
 
 
 
+// document.addEventListener("DOMContentLoaded", function() {
+//     var contactButton = document.getElementById("#bt1");
+//     var servicesButton = document.getElementById("#bt2");
+  
+//     contactButton.addEventListener("click", function() {
+//       // Scroll to the contact section
+//       var contactSection = document.getElementById("contact-section");
+//       contactSection.scrollIntoView({ behavior: "smooth" });
+//     });
+  
+//     servicesButton.addEventListener("click", function() {
+//       // Redirect to services.html
+//       window.location.href = "services.html";
+//     });
+//   });
+  
 
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var contactButton = document.getElementById("bt1");
+    var servicesButton = document.getElementById("bt2");
+  
+    contactButton.addEventListener("click", function() {
+      // Scroll to the contact section
+      var contactSection = document.getElementById("contact-section");
+      contactSection.scrollIntoView({ behavior: "smooth" });
+    });
+  
+    servicesButton.addEventListener("click", function() {
+      // Redirect to services.html
+      window.location.href = "services.html";
+    });
+  });
+  
+  let slides = document.querySelectorAll('.slide');
+  let currentIndex = 0;
+  
+  function showNextSlide() {
+      slides[currentIndex].classList.remove('active');
+      currentIndex = (currentIndex + 1) % slides.length;
+      slides[currentIndex].classList.add('active');
+  }
+  
+  setInterval(showNextSlide, 5000); // Adjust the slide duration (in milliseconds) as needed
+  
+  let heading = document.querySelector("#list");
+  
+  heading.addEventListener("mouseover", () => {
+      heading.style.backgroundColor = "green";
+  });
+  
