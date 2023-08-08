b1cf08363ef4a43d5d5b2e68fcc28dbbcf4fa075
Alex-gikungu <alexigikungu.012@gmail.com>
2023-08-08 18:37:56 +0300

    updated files

--- sripts.js
+++ sripts.js
@@ -1,43 +1,5 @@
-// let slides = document.querySelectorAll('.slide');
-// let currentIndex = 0;
 
-// function showNextSlide() {
-//     slides[currentIndex].classList.remove('active');
-//     currentIndex = (currentIndex + 1) % slides.length;
-//     slides[currentIndex].classList.add('active');
-// }
-
-// setInterval(showNextSlide, 5000); // Adjust the slide duration (in milliseconds) as needed
-
-// let heading = document.querySelector("#list");
-
-// heading.addEventListener("mouseover",()=>{
-//     heading.style.backgroundColor="green";
-// })
-
-
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     var contactButton = document.getElementById("#bt1");
-//     var servicesButton = document.getElementById("#bt2");
-  
-//     contactButton.addEventListener("click", function() {
-//       // Scroll to the contact section
-//       var contactSection = document.getElementById("contact-section");
-//       contactSection.scrollIntoView({ behavior: "smooth" });
-//     });
-  
-//     servicesButton.addEventListener("click", function() {
-//       // Redirect to services.html
-//       window.location.href = "services.html";
-//     });
-//   });
-  
-
-
-
-document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("DOMContentLoaded", function() {
     var contactButton = document.getElementById("bt1");
     var servicesButton = document.getElementById("bt2");
   
